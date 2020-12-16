d3a93e5083d94f96f57359f960547ba40b4003c0
myblindy <tweety.04.01@gmail.com>
2020-12-16 13:10:48 -0500

    basic raider & trait viewer UI

---
+++ rlm/Controls/TraitImage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+using rlm.Models;
+
+namespace rlm.Controls
+{
+    /// <summary>
+    /// Interaction logic for TraitImage.xaml
+    /// </summary>
+    public partial class TraitImage : UserControl
+    {
+        public Trait Trait
+        {
+            get => (Trait)GetValue(TraitProperty);
+            set => SetValue(TraitProperty, value);
+        }
+
+        public static readonly DependencyProperty TraitProperty = DependencyProperty.Register(nameof(Trait), typeof(Trait), typeof(TraitImage));
+
+        public TraitImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ rlm/Converters/VisibleIfNonZeroConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace rlm.Converters
+{
+    class VisibleIfNonZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is double dValue && dValue != 0 ? Visibility.Visible :
+            value is int iValue && iValue != 0 ? Visibility.Visible :
+            Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
