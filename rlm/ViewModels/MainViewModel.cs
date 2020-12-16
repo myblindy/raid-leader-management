6426305a5e22238211eaeb6463eb38dda99cfbe0
myblindy <tweety.04.01@gmail.com>
2020-12-16 16:33:38 -0500

    more classes, can sort by role

--- rlm/ViewModels/MainViewModel.cs
+++ rlm/ViewModels/MainViewModel.cs
@@ -19,6 +19,7 @@ namespace rlm.ViewModels
                 new() { Name = "Wired", Stats = { Reflexes = 4 } },
                 new() { Name = "Slow", Stats = { Reflexes = -1 } },
                 new() { Name = "Smooth", Stats = { Charisma = 5 } },
+                new() { Name = "Reckless", Stats = {Intelligence = -1, Reflexes = 2 } },
             }.ToDictionary(w => w.Name);
 
         public Dictionary<string, Class> AllClasses =
@@ -31,11 +32,15 @@ namespace rlm.ViewModels
                 new("Cleric",
                     new Specialization( "Holy", RaiderRoles.Healer ),
                     new Specialization( "Shadow", RaiderRoles.RangedDamage ),
-                    new Specialization( "Battle Cleric", RaiderRoles.MeleeDamage )),
+                    new Specialization( "Battle", RaiderRoles.MeleeDamage )),
                 new("Stabber",
                     new Specialization( "Assassin", RaiderRoles.MeleeDamage ),
                     new Specialization( "Thief", RaiderRoles.MeleeDamage ),
                     new Specialization( "Arcane Shadow", RaiderRoles.RangedDamage )),
+                new("Paladin",
+                    new Specialization( "Protector", RaiderRoles.Tank ),
+                    new Specialization( "Holy", RaiderRoles.Healer ),
+                    new Specialization( "Battle", RaiderRoles.MeleeDamage )),
             }.ToDictionary(w => w.Name);
 
         public ObservableCollection<Raider> Raiders { get; }
@@ -56,6 +61,7 @@ namespace rlm.ViewModels
             Raiders = new()
             {
                 new("John", "Fighter", "Protector", AllClasses) { Traits = { AllTraits["Analytical"], AllTraits["Wired"] } },
+                new("Thomas", "Paladin", "Protector", AllClasses) { Traits = { AllTraits["Reckless"], AllTraits["Wired"] } },
                 new("Timmy", "Stabber", "Arcane Shadow", AllClasses) { Traits = { AllTraits["Smooth"] } },
                 new("Alice", "Cleric", "Holy", AllClasses) { Traits = { AllTraits["Slow"] } },
             };
